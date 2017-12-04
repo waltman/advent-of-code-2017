f2d75df7475e65033e58432b8d4468eed96d8e86
Walt Mankowski <waltman@pobox.com>
2017-12-04 07:50:35 -0500

    python code for day 4

---
+++ day04/high_entropy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from sys import argv
+
+def is_valid(line):
+    s = set()
+    for pw in line.split():
+        if pw in s:
+            return 0
+        else:
+            s.add(pw)
+            
+    return 1
+    
+def is_valid2(line):
+    s = set()
+    for pw in line.split():
+        key = ''.join(sorted(list(pw)))
+        if key in s:
+            return 0
+        else:
+            s.add(key)
+            
+    return 1
+    
+
+filename = argv[1]
+sum1 = 0
+sum2 = 0
+
+with open(filename) as f:
+    for line in f:
+        if is_valid(line):
+            sum1 += 1
+        if is_valid2(line):
+            sum2 += 1
+
+print("result1:", sum1)
+print("result2:", sum2)
