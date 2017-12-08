b093ccb7f324d18a901bdc1ee53ab0f4709f911b
Walt Mankowski <waltman@pobox.com>
2017-12-08 16:44:41 -0500

    python code for day 8
    Turns out python has separate eval() and exec() functions! Who knew?

---
+++ day08/registers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from sys import argv
+
+reg = {}
+highest = -1e300
+filename = argv[1]
+with open(filename) as f:
+    for line in f:
+        line = line.rstrip("\n")
+        tok = line.split(" ")
+
+        # create registers if they don't exist
+        if tok[0] not in reg:
+            reg[tok[0]] = 0
+        if tok[4] not in reg:
+            reg[tok[4]] = 0
+
+        # create command
+        tok[0] = "reg['{}']".format(tok[0]);
+        tok[4] = "reg['{}']".format(tok[4]);
+        tok[1] = "+=" if tok[1] == "inc" else "-="
+        cmd = "if {} {} {}: {} {} {}".format(tok[4], tok[5], tok[6], tok[0], tok[1], tok[2])
+        exec(cmd)
+        high = max(reg.values())
+        highest = max(high, highest)
+
+print("result1:", max(reg.values()))
+print("result2:", highest)
