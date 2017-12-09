794f41934e792d306e6a22bd19fc3302be792526
Walt Mankowski <waltman@pobox.com>
2017-12-09 10:46:25 -0500

    python code for day 9

---
+++ day09/streams.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python 3
+from sys import argv
+import re
+
+p = re.compile('<([^>]*)>')
+filename = argv[1]
+with open(filename) as f:
+    for line in f:
+        # remove newline
+        line = line.rstrip("\n")
+        
+        # remove cancels
+        line = re.sub('!.', '', line)
+
+        # remove garbage
+        garbage_len = 0
+        m = p.search(line)
+        while m is not None:
+            garbage_len += len(m.group(1))
+            line = p.sub('', line, count=1)
+            m = p.search(line)
+
+        # compute score
+        score = 0
+        level = 0
+        for c in line:
+            if c == '{':
+                level += 1
+            elif c == '}':
+                score += level
+                level -= 1
+
+        print("score =", score)
+        print("garbage length =", garbage_len)
+        
