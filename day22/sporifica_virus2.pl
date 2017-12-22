166063556dd34d04613e543a13100c972d909abf
Walt Mankowski <waltman@pobox.com>
2017-12-22 13:52:32 -0500

    simplified the logic for the part 2 perl code
    I came up with a cleaner way to solve it in the python version, and
    I'm backporting it to the perl code.

--- day22/sporifica_virus2.pl
+++ day22/sporifica_virus2.pl
@@ -7,10 +7,9 @@ use experimental qw(signatures);
 my $ITER = shift @ARGV;
 my $SIZE = 9999;
 my $g;
-my %init_infect;
 for my $row (0..$SIZE-1) {
     for my $col (0..$SIZE-1) {
-        $g->[$row][$col] = 0;
+        $g->[$row][$col] = '.';
     }
 }
 
@@ -21,90 +20,88 @@ while (<>) {
     my $row = $. - 1 + $OFFSET;
     for my $i (0..$#val) {
         my $col = $i + $OFFSET;
-        if ($val[$i] eq '.') {
-            $g->[$row][$col] = 0;
-        } else {
-            $g->[$row][$col] = 1;
-            $init_infect{"$row,$col"} = 1;
-        }
+        $g->[$row][$col] = $val[$i];
     }
 }
 
 my $cnt = 0;
-my ($drow, $dcol) = (-1, 0);
+my $d = 'u';
 my ($row, $col) = (int($SIZE/2), int($SIZE/2));
 for my $it (1..$ITER) {
     my $val = $g->[$row][$col];
-    if ($val == 0) { # clean
-        ($drow, $dcol) = turn_left($drow, $dcol);
-        $g->[$row][$col] = 2; # weakened
-    } elsif ($val == 1) { # infected
-        ($drow, $dcol) = turn_right($drow, $dcol);
-        $g->[$row][$col] = 3; # flagged
-    } elsif ($val == 2) { # weakened
-        $g->[$row][$col] = 1; # infected
+    if ($val eq '.') {
+        $d = turn_left($d);
+        $g->[$row][$col] = 'W';
+    } elsif ($val eq '#') {
+        $d = turn_right($d);
+        $g->[$row][$col] = 'F';
+    } elsif ($val eq 'W') {
+        $g->[$row][$col] = '#'; # infected
         $cnt++;
-    } elsif ($val == 3) { # flagged
-        ($drow, $dcol) = turn_reverse($drow, $dcol);
-        $g->[$row][$col] = 0; # clean
+    } elsif ($val eq 'F') { # flagged
+        $d = turn_reverse($d);
+        $g->[$row][$col] = '.'; # clean
     }
+    my ($drow, $dcol) = d2move($d);
     $row += $drow;
     $col += $dcol;
 }
-say "result1: $cnt";
+say "result2: $cnt";
 
 sub printg($g) {
     for my $row (0..$#$g) {
         for my $col (0..$#$g) {
-            my $val = $g->[$row][$col];
-            if ($val == 0) {
-                print ". ";
-            } elsif ($val == 1) {
-                print "# ";
-            } elsif ($val == 2) {
-                print "W ";
-            } elsif ($val == 3) {
-                print "F ";
-            }
+            print "$g->[$row][$col] ";
         }
         print "\n";
     }
     print "\n";
 }
 
-sub turn_right($dx, $dy) {
-    if ($dx == -1) { # up
-        return (0, 1); # right
-    } elsif ($dx == 1) { # down
-        return (0, -1); # left
-    } elsif ($dy == -1) { # left
-        return (-1, 0); # up
-    } else { # right
-        return (1, 0); # down
+sub turn_right($d) {
+    if ($d eq 'u') {
+        return 'r';
+    } elsif ($d eq 'r') {
+        return 'd';
+    } elsif ($d eq 'd') {
+        return 'l';
+    } else {
+        return 'u';
     }
 }
 
-sub turn_left($dx, $dy) {
-    if ($dx == -1) { # up
-        return (0, -1); # left
-    } elsif ($dx == 1) { # down
-        return (0, 1); # right
-    } elsif ($dy == -1) { # left
-        return (1, 0); # down
-    } else { # right
-        return (-1, 0); # up
+sub turn_left($d) {
+    if ($d eq 'u') {
+        return 'l';
+    } elsif ($d eq 'l') {
+        return 'd';
+    } elsif ($d eq 'd') {
+        return 'r';
+    } else {
+        return 'u';
     }
 }
 
-sub turn_reverse($dx, $dy) {
-    if ($dx == -1) { # up
-        return (1, 0); # down
-    } elsif ($dx == 1) { # down
-        return (-1, 0); # up
-    } elsif ($dy == -1) { # left
-        return (0, 1); # right
-    } else { # right
-        return (0, -1); # left
+sub turn_reverse($d) {
+    if ($d eq 'u') {
+        return 'd';
+    } elsif ($d eq 'l') {
+        return 'r';
+    } elsif ($d eq 'd') {
+        return 'u';
+    } else {
+        return 'l';
     }
 }
 
+sub d2move($d) {
+    if ($d eq 'u') {
+        return (-1, 0);
+    } elsif ($d eq 'r') {
+        return (0, 1);
+    } elsif ($d eq 'd') {
+        return (1, 0);
+    } else {
+        return (0, -1);
+    }
+}
